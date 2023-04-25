5cb333c21e1ae4b6d70edd780611ee31e06e64c2
Kelemen Péter <peter.kelemen456@gmail.com>
2023-04-25 21:17:57 +0200

    Created initial state for board

--- src/main/java/boardgame/BoardGameController.java
+++ src/main/java/boardgame/BoardGameController.java
@@ -16,21 +16,40 @@ public class BoardGameController {
     private void initialize() {
         for (var i = 0; i < board.getRowCount(); i++) {
             for (var j = 0; j < board.getColumnCount(); j++) {
-                var square = createSquare();
+                var square = createSquare(i,j);
                 board.add(square, j, i);
             }
         }
     }
 
-    private StackPane createSquare() {
+    private StackPane createSquare(int i, int j) {
+
         var square = new StackPane();
         square.getStyleClass().add("square");
         var piece = new Circle(50);
         piece.setFill(Color.TRANSPARENT);
         square.getChildren().add(piece);
-        square.setOnMouseClicked(this::handleMouseClick);
+        square.setOnMouseClicked(this::handleMouseClick); // MOUSE CLICK HANDLER
+
+
+        if(i == 0){
+            piece.setFill(Color.RED);
+        }
+
+        if(i == 5){
+            piece.setFill(Color.BLUE);
+        }
+
+        if((i == 2 && j == 4) || (i == 3 && j == 2)){
+            piece.setFill(Color.TRANSPARENT);
+        }
+
         return square;
+
     }
+
+
+
 
     @FXML
     private void handleMouseClick(MouseEvent event) {
